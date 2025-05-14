608226bcd236dd24706409ddb588e4e3d6fda945
youna0123 <kyaa123@naver.com>
2025-05-15 08:52:03 +0900

    Delete main directory

--- main/java/team/suajung/ad/ress/auth/exception/JwtAuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package team.suajung.ad.ress.auth.exception;
-
-public class JwtAuthenticationException extends RuntimeException {
-    public JwtAuthenticationException(String message) {
-        super(message);
-    }
-}
